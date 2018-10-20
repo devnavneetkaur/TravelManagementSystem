4a375cdcf782bf38e35ba798ee21743e49518525
Navneet Kaur <dev.navneetkaur@gmail.com>
2018-10-20 21:26:50 +0530

    TMapp: Updated Login page to use Jpanel
    Login.java modified to use Jpane

--- src/com/login/Login.java
+++ src/com/login/Login.java
@@ -19,6 +19,7 @@ public class Login extends JFrame/* implements ActionListener */ {
 
 	Login() {
 
+		panel = new JPanel();
 		login_label = new Mylabel("login");
 		Font font = new Font("Cooper Black", Font.BOLD, 30);
 		login_label.setFont(font);
@@ -28,37 +29,37 @@ public class Login extends JFrame/* implements ActionListener */ {
 		login_label.setBackground(Color.BLUE);
 		login_label.setBorder(BorderFactory.createLineBorder(Color.WHITE));
 		login_label.setHorizontalAlignment(login_label.CENTER);
-		add(login_label);
+		panel.add(login_label);
 
 		email_label = new Mylabel("Enter Email");
 		email_label.setBounds(10, 100, 120, 25);
-		add(email_label);
+		panel.add(email_label);
 		email_textfield = new Mytextfield();
 		email_textfield.setBounds(140, 100, 150, 25);
-		add(email_textfield);
+		panel.add(email_textfield);
 		email_textfield.addKeyListener((KeyListener) email_textfield);
 
 		forgot_email_btn = new JButton("Forgot email?");
 		forgot_email_btn.setForeground(Color.BLUE);
 		forgot_email_btn.setContentAreaFilled(false);
 		forgot_email_btn.setBorder(null);
-		add(forgot_email_btn);
+		panel.add(forgot_email_btn);
 		forgot_email_btn.setBounds(10, 130, 80, 35);
 
 		password_label = new Mylabel("Enter password");
 		password_label.setBounds(10, 170, 120, 25);
-		add(password_label);
+		panel.add(password_label);
 		password_field = new Mypasswordfield();
 		password_field.setEchoChar('*');
 		password_field.setBounds(140, 170, 150, 25);
-		add(password_field);
+		panel.add(password_field);
 		password_field.addKeyListener((KeyListener) password_field);
 
 		forgot_pswd_btn = new JButton("Forgot pswd?");
 		forgot_pswd_btn.setForeground(Color.BLUE);
 		forgot_pswd_btn.setContentAreaFilled(false);
 		forgot_pswd_btn.setBorder(null);
-		add(forgot_pswd_btn);
+		panel.add(forgot_pswd_btn);
 		forgot_pswd_btn.setBounds(10, 200, 80, 35);
 
 		create_account_btn = new MyButton("Create Account");
@@ -67,7 +68,7 @@ public class Login extends JFrame/* implements ActionListener */ {
 		Font f = new Font("Cooper Black", Font.BOLD, 15);
 		create_account_btn.setFont(f);
 		create_account_btn.setBorder(null);
-		add(create_account_btn);
+		panel.add(create_account_btn);
 		create_account_btn.setBounds(10, 260, 130, 35);
 		create_account_btn.addActionListener((ActionListener) create_account_btn);
 
@@ -78,16 +79,21 @@ public class Login extends JFrame/* implements ActionListener */ {
 		login_btn.setFont(new Font("Cooper Black", Font.BOLD, 15));
 	    login_btn.addActionListener((ActionListener) login_btn);
 
-		add(login_btn);
+		panel.add(login_btn);
 		login_btn.setBounds(160, 260, 90, 35);
-
-		setLayout(null);
+		add(panel);
+		
 		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
-		setVisible(true);
+		
 		setResizable(false);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		panel.setBounds(0, 0, 400, 500);
 		setBounds(0, 0, 400, 500);
 		setLocation((int) (d.width / 2 - getSize().getWidth() / 2), (int) (d.height / 2 - getSize().getHeight() / 2));
+		panel.setLayout(null);
+		setLayout(null);
+		panel.setVisible(true);
+		setVisible(true);
 	}
 
 	public static void main(String[] args) {
